e7f5c9ddbb8d2294235128d08f9f64b32819bd05
starychuj <66310204+starychuj@users.noreply.github.com>
2021-02-01 09:08:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,16 +12,21 @@ async def on_ready():
 async def on_message(message):
    if message.content.startswith('adam'):
       await message.channel.send(msg[random.randint(0,len(msg)-1)])
-      
+      return
+   
    if message.content.startswith('$text'):
       await message.channel.send(text)
-   
+      return
    if message.content.startswith('$kurwin'):
       await message.channel.send(kurwin)
+      return
    
    if message.content.startswith('$dzierg2'):
       await message.channel.send(dzierg2)
+      return
    
    if message.content.startswith('$dzierg'):
       await message.channel.send(dzierg)
+      return
+   
 client.run(token)
